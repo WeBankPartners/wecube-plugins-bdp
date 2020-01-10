ee120b114eee1fd576b5e898a31889548393ca01
HoweChen <yuhao.howe.chen@gmail.com>
2020-01-10 17:14:56 +0800

    #8 update scaffold

--- src/main/java/com/webank/wecube/plugins/bdp/common/ApplicationProperties.java
+++ src/main/java/com/webank/wecube/plugins/bdp/common/ApplicationProperties.java
@@ -7,13 +7,40 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @ConfigurationProperties(prefix = "wecube.plugins.bdp")
 public class ApplicationProperties {
-    private String server;
+    private String host;
+    private String port;
+    private String username;
+    private String password;
 
-    public String getServer() {
-        return server;
+    public String getHost() {
+        return host;
     }
 
-    public void setServer(String server) {
-        this.server = server;
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
