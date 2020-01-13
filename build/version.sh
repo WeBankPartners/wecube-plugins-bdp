14201ea0925d11a3fde77783df2dc701d6339bfa
HoweChen <yuhao.howe.chen@gmail.com>
2020-01-13 17:39:46 +0800

    #8 update .gitignore and add build files

--- build/version.sh
+++ build/version.sh
@@ -7,15 +7,14 @@
 COMMIT=$(git rev-parse --short HEAD)
 GIT_TAG=$(git tag -l --contains HEAD | head -n 1)
 
-
 if [[ -z "$DIRTY" && -n "$GIT_TAG" ]]; then
-    VERSION=$GIT_TAG
+  VERSION=$GIT_TAG
 else
-    VERSION="${COMMIT}${DIRTY}"
+  VERSION="${COMMIT}${DIRTY}"
 fi
 
-if [ -z $VERSION ];then
-    VERSION="dev"
+if [ -z $VERSION ]; then
+  VERSION="dev"
 fi
 
 echo $VERSION
